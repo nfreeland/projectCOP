6c31042865080b352f62a1e4924accdaab371640
Roger Johnson <rkj8@students.uwf.edu>
2021-10-10 18:54:11 -0500

    Update FinalSubmission.sql

--- FinalSubmission.sql
+++ FinalSubmission.sql
@@ -1,4 +1,19 @@
 -- Create all tables and run a test
 @CreateTables;
+
+-- Grab table Descriptions
+DESC Project;
+DESC Lab;
+DESC Firm;
+DESC Equipment;
+DESC Station;
+DESC Sample;
+DESC Holds;
+DESC Test;
+DESC Design;
+DESC Research;
+
+@
+
 @PopulateTables;
 @TestTables;
