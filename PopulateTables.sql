00f1d2d60ca8d1c72b729ff4b5045d16aaf258a6
Roger Johnson <rkj8@students.uwf.edu>
2021-10-10 18:39:07 -0500

    Update PopulateTables.sql

--- PopulateTables.sql
+++ PopulateTables.sql
@@ -1,8 +1,8 @@
 -- Populate each table with the necessary data
 
 
-INSERT INTO Project VALUES ('projectOne', 'investigatorOne', TO_DATE('2012-06-05'));
-INSERT INTO Project VALUES ('projectTwo', 'investigatorTwo', TO_DATE('2012-08-12'));
-INSERT INTO Project VALUES ('projectThree', 'investigatorOne', TO_DATE('2012-03-16'));
-INSERT INTO Project VALUES ('projectFour', 'investigatorThree', TO_DATE('2012-01-11'));
-INSERT INTO Project VALUES ('projectFive', 'investigatorThree', TO_DATE('2012-11-12'));
+INSERT INTO Project VALUES ('projectOne', 'investigatorOne', TO_DATE('2012-06-05', 'YYYY-MM-dd'));
+INSERT INTO Project VALUES ('projectTwo', 'investigatorTwo', TO_DATE('2012-08-12', 'YYYY-MM-dd'));
+INSERT INTO Project VALUES ('projectThree', 'investigatorOne', TO_DATE('2012-03-16', 'YYYY-MM-dd'));
+INSERT INTO Project VALUES ('projectFour', 'investigatorThree', TO_DATE('2012-01-11', 'YYYY-MM-dd'));
+INSERT INTO Project VALUES ('projectFive', 'investigatorThree', TO_DATE('2012-11-12', 'YYYY-MM-dd'));
