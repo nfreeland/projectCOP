69cdf9c3ac31fd6863b64ff4e1eef73ca23395c7
Roger Johnson <rkj8@students.uwf.edu>
2021-10-09 20:54:14 -0500

    Update CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -16,14 +16,22 @@ date_began DATE
 CREATE TABLE Lab(
 lab_number NUMBER(3) NOT NULL PRIMARY KEY,
 lab_manager VARCHAR2(64),
-lab_phone VARCHAR2(16) -- or NUMBER(9) ?
+lab_phone VARCHAR2(16) 
 );
 
 -- Firm(Firm_Name pk, Address, Firm_Phone)
 CREATE TABLE Firm(
 firm_name VARCHAR2(64) NOT NULL PRIMARY KEY,
 address VARCHAR2(64),
-firm_phone VARCHAR2(16) -- or NUMBER(9) ?
+firm_phone VARCHAR2(16) 
+);
+
+-- Equipment(Equipment_Name, Equipment_designation, Firm_Name)
+CREATE TABLE Equipment(
+equipment_name VARCHAR2(16) NOT NULL PRIMARY KEY,
+equipment_designation VARCHAR2(16), 
+firm_name VARCHAR2(64),
+FOREIGN KEY (firm_name) REFERENCES Firm(firm_name)
 );
 
 -- Station(Location_ID pk, Gps_coords, project_name fk)
@@ -38,21 +46,13 @@ FOREIGN KEY (project_name) REFERENCES Project(project_name)
 
 -- Sample(Sample_ID pk, Sample_code, Sample_Type, Sample_date, Firm_name fk, proje$
 CREATE TABLE Sample(
-sample_id NUMBER(3) NOT NULLL PRIMARY KEY,
+sample_id NUMBER(3) NOT NULL PRIMARY KEY,
 sample_code NUMBER(3),
-sample_type NUMBER(3), -- or VARCHAR2(8) ?
+sample_type NUMBER(3), 
 sample_date DATE,
 firm_name VARCHAR2(64),
 project_name VARCHAR2(64),
 FOREIGN KEY (project_name) REFERENCES Project(project_name),
-FOREIGN KEY (firm_name) REFERENCES Firm(firm_name)
-);
-
--- Equipment(Equipment_Name, Equipment_designation, Firm_Name)
-CREATE TABLE Equipment(
-equipment_name VARCHAR2(16) NOT NULL PRIMARY KEY,
-equipment_designation VARCHAR2(16) -- or NUMBER(8) ?
-firm_name VARCHAR2(64),
 FOREIGN KEY (firm_name) REFERENCES Firm(firm_name)
 );
 
