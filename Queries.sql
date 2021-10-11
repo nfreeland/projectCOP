e7db468b6dc03f34fbecc1996a01be59f4d83521
Roger Johnson <rkj8@students.uwf.edu>
2021-10-10 19:02:33 -0500

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -4,3 +4,10 @@ WHERE S.sample_id=T.sample_ID
 AND T.Lab_number<10
 -- From this querry we expect to get sample_codes for test by lab numbers less than 10
 ;
+
+SELECT location_id
+FROM Holds h, Equipment e
+WHERE b.equipment_name = e.equipment_name
+AND b.equipment_name LIKE '%OMatic%'
+ORDER BY location_id
+; 
