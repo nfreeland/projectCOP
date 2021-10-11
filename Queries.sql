f17ad4a83b548786d27e84fb22b6e7572ce7a317
nfreeland <70666467+nfreeland@users.noreply.github.com>
2021-10-10 18:03:24 -0600

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -11,3 +11,17 @@ WHERE b.equipment_name = e.equipment_name
 AND b.equipment_name LIKE '%OMatic%'
 ORDER BY location_id
 ; 
+
+SELECT L.Lab_Manager, T.Sample_ID
+FROM Test T, Lab L
+WHERE L.Lab_number=T.Lab_number
+AND T.Lab_Number<10
+-- From this query we expect to get the Lab Manager and Sample_IDs for test done by labs with lab numbers less than 10
+; 
+
+Select E.Eqipment_Designation
+FROM Holds H, Equipment E
+WHERE E.Equipment_Name=H.Equipment_Name
+AND rownum<11
+-- From this query we expect to get the Equipment Designation for the first 10 results of equipment that is being Held
+;
